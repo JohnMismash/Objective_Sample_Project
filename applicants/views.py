15d4c30e49192ff6f8872d23b85b67368fd0b9d5
JohnMismash <u1179865@utah.edu>
2022-05-12 17:13:59 -0600

    Created classes for each attribute.

--- applicants/views.py
+++ applicants/views.py
@@ -1,32 +1,106 @@
+from unicodedata import name
 from django.http import HttpResponse
 from django.shortcuts import render
 import json
 
 class Summary:
-    def __init__(self, applicants, jobs, skills) -> None:
-        self.applicants = applicants
-        self.jobs = jobs
-        self.skills = skills
-    
-    @staticmethod
-    def from_JSON(json_string):
-        json_dict = json.loads(json_string)
-        return Summary(**json_dict)
+    def __init__(self, summary):
+       self.applicants_json = summary['applicants']
+       self.jobs_json = summary['jobs']
+       self.skills_json = summary['skills']
 
-    def to_HTML(self):
-        return f'Job Applicants: {self.applicants}'
+       self.applicants = []
+       self.jobs = []
+       self.skills = []
+
+       for job in self.jobs_json:
+            id = job['id']
+            name = job['name']
+
+            current = Job(id, name)
+            self.jobs.append(current)
+
+       for skill in self.skills_json:
+            id = skill['id']
+            name = skill['name']
+            applicant_id = skill['applicant_id']
+
+            current = Skills(id, name, applicant_id)
+            self.skills.append(current)
+
+       for applicant in self.applicants_json:
+           id = applicant['id']
+           name = applicant['name']
+           email = applicant['email']
+           website = applicant['website']
+           cover_letter = applicant['cover_letter']
+           job_id = applicant['job_id']
+
+           current = Applicant(id, name, email, website, cover_letter, job_id)
+           self.applicants.append(current)
+
+    @staticmethod
+    def from_json(json_string):
+        json_dict = json.loads(json_string)
+        # print(json_dict)
+        return Summary(json_dict)
+
+    # Debugging
+    def __ref__(self):
+        print('')
+
+        for applicant in self.applicants:
+            print(f'Applicant: {applicant.id}')
+            print(applicant.name)
+            print(applicant.email)
+            print(applicant.website)
+            print(applicant.cover_letter)
+            print(f'Job ID: {applicant.job_id}')
+            print('')
+
+        for job in self.jobs:
+            print(f'Job: {job.id}')
+            print(job.name)
+            print('')
+
+        for skill in self.skills:
+            print(f'Skill: {skill.id}')
+            print(skill.name)
+            print(f'Applicant ID: {skill.applicant_id}')
+            print('')
+
+
+class Applicant:
+    def __init__(self, id, name, email, website, cover_letter, job_id):
+        self.id = id
+        self.name = name
+        self.email = email
+        self.website = website
+        self.cover_letter = cover_letter
+        self.job_id = job_id
+
+class Job:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+
+class Skills:
+    def __init__(self, id, name, applicant_id):
+        self.id = id
+        self.name = name
+        self.applicant_id = applicant_id
 
 
 # Create your views here.
 
 def applicants_view_json(request):
-    file = open('data.json', "r")
-    json_string = file.read()
-
-    summary = Summary.from_JSON(json_string)
-    file = open('JSONView.html', "w")
-    file.write(summary.to_HTML())
-    file.close()
+    with open('data.json', "r") as json_file:
+        summary = Summary.from_json(json_file.read())
+        summary.__ref__()
+            
+        #file = open('JSONView.html', "w")
+        #file.write(summary)
+        #ile.close()
 
     return render(request, 'JSONView.html')
 
