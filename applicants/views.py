d3e306d2de34f967a0f295c87b79fa20d0855e4e
JohnMismash <u1179865@utah.edu>
2022-05-12 15:31:33 -0600

    Created a class for the summary of applicants.

--- applicants/views.py
+++ applicants/views.py
@@ -1,11 +1,33 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import json
 
-from django.shortcuts import render
+class Summary:
+    def __init__(self, applicants, jobs, skills) -> None:
+        self.applicants = applicants
+        self.jobs = jobs
+        self.skills = skills
+    
+    @staticmethod
+    def from_JSON(json_string):
+        json_dict = json.loads(json_string)
+        return Summary(**json_dict)
+
+    def to_HTML(self):
+        return f'Job Applicants: {self.applicants}'
+
 
 # Create your views here.
 
 def applicants_view_json(request):
+    file = open('data.json', "r")
+    json_string = file.read()
+
+    summary = Summary.from_JSON(json_string)
+    file = open('JSONView.html', "w")
+    file.write(summary.to_HTML())
+    file.close()
+
     return render(request, 'JSONView.html')
 
 def applicants_view_sql(request):
@@ -16,3 +38,4 @@ def applicants_view_db(request):
 
 def applicants_view_ref(request):
     return render(request, 'index.html')
+
